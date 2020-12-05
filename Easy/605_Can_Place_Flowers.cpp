92108763b70f6f07042455991781c268b240cb32
Shehab Abdel-Salam <shehab_m@aucegypt.edu>
2020-12-05 12:02:37 +0200

    5 dec

--- Easy/605_Can_Place_Flowers.cpp
+++ Easy/605_Can_Place_Flowers.cpp
@@ -3,12 +3,12 @@ public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         flowerbed.insert(flowerbed.begin(), 0);
         flowerbed.push_back(0);
-        for(int i = 1; i < flowerbed.size() - 1; i++) {
-            if(flowerbed[i-1] + flowerbed[i] + flowerbed[i+1] == 0) {
+        for(int i = 1; i < flowerbed.size() - 1 && n; i++) {
+            if(flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0) {
+                flowerbed[i] = 1;
                 n--;
-                i++;
             }
         }
-        return n <= 0;
+        return n == 0 ? true : false;
     }
 };
